9d65e524752299fade37968afd588363c2829712
Vinicius Brandao <vinicius.brandaot@gmail.com>
2024-02-24 15:21:55 -0300

    import data from csv

---
+++ manipulate_data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from datetime import datetime 
+
+machines = pd.read_csv('machine_power.csv')
+
+# transformar as horas em um int 
+def time_to_hour(time_str):
+    hour = int(''.join(map(str, map(int, time_str.replace(':', '')))))
+    return hour
+
+# aplicar a transformação para todos os valores
+machines['inicio_uso_tipico'] = machines['inicio_uso_tipico'].apply(time_to_hour)
+machines['fim_uso_tipico'] = machines['fim_uso_tipico'].apply(time_to_hour)
+
+print(machines)
