2a9e2121161e155f074a18e2352b6eae29858aeb
Vinicius Brandao <vinicius.brandaot@gmail.com>
2024-02-24 20:40:16 -0300

    added ids

--- manipulate_data.py
+++ manipulate_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from datetime import datetime 
-
+from priority_queue import PriorityQueue
 
 machines = pd.read_csv('machine_power.csv')
 
@@ -16,5 +16,21 @@ machines['fim_uso_tipico'] = machines['fim_uso_tipico'].apply(time_to_hour)
 priorities = machines['prioridade']
 machines = machines.drop(['prioridade'], axis=1)
 
-print(priorities)
-print(machines)
+# Get the values of the DataFrame as a NumPy array
+machines_values = machines.values
+
+# Convert each row into a list and store them in a list
+machines = [list(row) for row in machines_values]
+
+
+
+organizer = PriorityQueue()
+
+organizer.add_list(machines, priorities)
+
+organizer.print_queue()
+
+# print(organizer.get_queue())
+
+
+
