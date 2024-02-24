9ac1bd1e22ad55e414289b9fa83c2c0660722f10
Vinicius Brandao <vinicius.brandaot@gmail.com>
2024-02-24 20:56:17 -0300

    comments

--- manipulate_data.py
+++ manipulate_data.py
@@ -9,28 +9,37 @@ def time_to_hour(time_str):
     hour = int(''.join(map(str, map(int, time_str.replace(':', '')))))
     return hour
 
-# aplicar a transformação para todos os valores
+# aplicar a transformação de datas em valores
 machines['inicio_uso_tipico'] = machines['inicio_uso_tipico'].apply(time_to_hour)
 machines['fim_uso_tipico'] = machines['fim_uso_tipico'].apply(time_to_hour)
 
+# separar prioridades dos items
 priorities = machines['prioridade']
-machines = machines.drop(['prioridade'], axis=1)
 
-# Get the values of the DataFrame as a NumPy array
+# tratar itens para ficarem em formato correto para lista
 machines_values = machines.values
-
-# Convert each row into a list and store them in a list
 machines = [list(row) for row in machines_values]
 
+'''
+============================================= IDEIA PRO PROJETO ===============================================
+a ideia que eu acho mais facil de lidar com nosso programa seria de depois que ler a lista original
+lidar só com listas do python, ao invés de salvar as coisas em um novo csv.
+A gente implementa alguma lógica para ir atualizando os itens corretamente.
+Os itens devem ter um critério baseado em suas caracteristicas para receberem uma nova prioridade
+Podemos usar o horário atual e funções assincronas que contam o tempo para atualizar a lista e retornar ela. 
+Pra fazer isso, temos que atualizar a lista baseado no id de cada item, para não desligar o que não devemos.
 
+===============================================================================================================
+'''
 
+# Alimentar itens para a lista (pode ser feita uma classe ou função para isso)
 organizer = PriorityQueue()
 
 organizer.add_list(machines, priorities)
 
 organizer.print_queue()
 
-# print(organizer.get_queue())
+
 
 
 
