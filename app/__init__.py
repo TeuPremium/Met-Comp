819751108bc5d487698da103cfc8e6b183c454bc
Jairo Magno C. Marques <Jairo.Magno@DSN03.direction.com.br>
2024-02-28 22:47:13 -0300

    Adicionando Estilos e funcionalidades com botões para mostrar tabelas com os ambientes ligados

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__)   #variável responsável por receber os atributos da Classe Flask para utilização.
 
-from app import routes
+from app import routes  #Importanto os scripts para integração web
