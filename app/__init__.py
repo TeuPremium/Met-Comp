96b16e6fa600c0feb6243ffa947ec113d865cbd4
Jairo Magno C. Marques <Jairo.Magno@DSN03.direction.com.br>
2024-03-19 19:43:45 -0300

    init change

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,4 @@ from flask import Flask
 
 app = Flask(__name__)   #variável responsável por receber os atributos da Classe Flask para utilização.
 
-from app import routes  #Importanto os scripts para integração web
+from app import routes  #Importando os scripts para integração web
