1b3aa8c38958169c7f150415f1993ddd5059dc75
teuprimo <vinicius.brandaot@gmail.com>
2024-03-20 00:50:34 -0300

    double csv readout

--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,9 @@
-# from app import app
-# from flask import render_template, request
-
-# from app.functions.manipulate_data import data_processing
-
-# @app.route('/', methods=['GET', 'POST']) #Decorades são responsável por informar qual a rota determinada página deve aparecer
-# @app.route('/index', methods=['GET', 'POST'])  
-# def main_page():      #Função da página principal
-#     show_table: bool = False
-#     filename: str = 'machine_power.csv'
-#     on_off: dict = data_processing(filename)
-
-#     if request.method == 'POST':
-#         if ('show-table' in request.form) and (request.form['show-table'] == 'firma01'):
-#             show_table = True
-
-#     return render_template('index.html', on_off=on_off, show_table=show_table)  #renderizando a página HTML 
-
 from app import app
 from flask import render_template, request
 
 from app.functions.manipulate_data import data_processing
 
-@app.route('/', methods=['GET', 'POST']) 
+@app.route('/', methods=['GET', 'POST']) #Decorades são responsável por informar qual a rota determinada página deve aparecer
 @app.route('/index', methods=['GET', 'POST'])  
 def main_page():
     show_table: bool = False
@@ -37,4 +19,4 @@ def main_page():
             on_off = data_processing(filename)
             show_table = True
 
-    return render_template('index.html', on_off=on_off, show_table=show_table)
+    return render_template('index.html', on_off=on_off, show_table=show_table)  #renderizando a página HTML 
