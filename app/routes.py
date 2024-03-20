de1a68c7aa38936f5b75edfd76510262f8749b49
teuprimo <vinicius.brandaot@gmail.com>
2024-03-20 00:49:42 -0300

    double csv readout

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,40 @@
+# from app import app
+# from flask import render_template, request
+
+# from app.functions.manipulate_data import data_processing
+
+# @app.route('/', methods=['GET', 'POST']) #Decorades são responsável por informar qual a rota determinada página deve aparecer
+# @app.route('/index', methods=['GET', 'POST'])  
+# def main_page():      #Função da página principal
+#     show_table: bool = False
+#     filename: str = 'machine_power.csv'
+#     on_off: dict = data_processing(filename)
+
+#     if request.method == 'POST':
+#         if ('show-table' in request.form) and (request.form['show-table'] == 'firma01'):
+#             show_table = True
+
+#     return render_template('index.html', on_off=on_off, show_table=show_table)  #renderizando a página HTML 
+
 from app import app
 from flask import render_template, request
 
 from app.functions.manipulate_data import data_processing
 
-@app.route('/', methods=['GET', 'POST']) #Decorades são responsável por informar qual a rota determinada página deve aparecer
+@app.route('/', methods=['GET', 'POST']) 
 @app.route('/index', methods=['GET', 'POST'])  
-def main_page():      #Função da página principal
+def main_page():
     show_table: bool = False
     filename: str = 'machine_power.csv'
     on_off: dict = data_processing(filename)
 
     if request.method == 'POST':
-        if ('show-table' in request.form) and (request.form['show-table'] == 'firma01'):
+        if 'show-table' in request.form:
+            if request.form['show-table'] == 'firma01':
+                filename = 'machine_power.csv'
+            elif request.form['show-table'] == 'firma02':
+                filename = 'machine_power2.csv'
+            on_off = data_processing(filename)
             show_table = True
 
-    return render_template('index.html', on_off=on_off, show_table=show_table)  #renderizando a página HTML 
+    return render_template('index.html', on_off=on_off, show_table=show_table)
